1003adf6f161f63548425d7f3dabbd95d971aefe
Nadya Aurelia H <nadyaaureliah@gmail.com>
2022-09-19 03:36:41 +1000

    View Toggle test component passed

--- client/src/components/__test__/ViewToggle.test.js
+++ client/src/components/__test__/ViewToggle.test.js
@@ -1,23 +1,27 @@
 import React from "react";
-import viewToggle from "../viewToggle";
+import ViewToggle from "../viewToggle";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import renderer from 'react-test-renderer';
 
-test("viewToggle slider renders with correct elements", () => {
-    const { getByTestId } = render(<viewToggle />);
-    const toggleEl = getByTestId("viewToggle");
-
-    expect();
-});
-
-test("viewToggle slider renders with correct styling", () => {
-
+// Snapshot test to account for changes to the UI, whether expected or unexpected
+test("viewToggle slider renders correctly", () => {
+    const viewToggle = renderer.create(<ViewToggle />).toJSON();
+    expect(viewToggle).toMatchSnapshot();
 });
 
 test("viewToggle slider contains initial input value of false", () => {
+    const { getByTestId } = render(<ViewToggle />);
+    const checkEl = getByTestId("check-viewToggle");
+    expect(checkEl.checked).toEqual(false);
 
 });
 
 test("viewToggle slider contains input value of true on click", () => {
+    const { getByTestId } = render(<ViewToggle />);
+    const checkEl = getByTestId("check-viewToggle");
+    const sliderEl = getByTestId("slider-viewToggle");
 
+    fireEvent.click(sliderEl);
+    expect(checkEl.checked).toEqual(true);
 });
--- client/src/components/viewToggle.js
+++ client/src/components/viewToggle.js
@@ -5,10 +5,10 @@ import "./viewToggle.css";
 const viewToggle = ({isToggled, onToggle}) => {
     return (
         <label className="viewToggle" data-testid="viewToggle">
-            <input type="checkbox" checked = {isToggled} onChange = {onToggle}/>
-            <span className="slider">
-                <Icon icon="clarity:grid-view-line" className="view-icon"/>
-                <Icon icon="system-uicons:menu-hamburger" className="view-icon"/>
+            <input type="checkbox" data-testid="check-viewToggle" checked = {isToggled} onChange = {onToggle}/>
+            <span className="slider" data-testid="slider-viewToggle">
+                <Icon icon="clarity:grid-view-line" className="view-icon" data-testid="gallery-viewToggle"/>
+                <Icon icon="system-uicons:menu-hamburger" className="view-icon" data-testid="list-viewToggle"/>
             </span>
         </label>
     );
