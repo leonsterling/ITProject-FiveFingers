3a3e6d20242ff7af3ac36bd606736135324daa46
leonia <leonia@student.unimelb.edu.au>
2022-09-18 15:36:07 +1000

    attempt to push heroku

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.listen(process.env.PORT || 5100, () => {
 })
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 // connect mongoose index in models folder
