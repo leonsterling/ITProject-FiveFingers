13fb02771d0fc6a2b26921abc911aa244989e141
leonia <leonia@student.unimelb.edu.au>
2022-09-18 17:23:16 +1000

    preparing for heroku deployment testing

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.listen(process.env.PORT || 5100, () => {
     console.log('Server is alive!')
 })
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 // connect mongoose index in models folder
 require('./models')
 
