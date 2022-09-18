dd4e187f3ad3539dfeb3c51bc6e48c12f2ec4c4c
leonia <leonia@student.unimelb.edu.au>
2022-09-18 18:51:02 +1000

    changed localhost link

--- client/src/pages/Login/LoginForm.js
+++ client/src/pages/Login/LoginForm.js
@@ -35,7 +35,7 @@ export default function LoginForm () {
       setLoginState({validLogin: false});
       const configuration = {
         method: "post",
-        url: "http://localhost:5100/login",
+        url: "https://sterlingfamilyartefacts.herokuapp.com/login",
         data: {
           username,
           password,
