3eb2364113123553692f3e8c79554fad743e043b
leonia <leonia@student.unimelb.edu.au>
2022-09-19 15:58:15 +1000

    test for full view

--- client/src/pages/FullView/FullView.js
+++ client/src/pages/FullView/FullView.js
@@ -92,7 +92,11 @@ function FullView() {
             />
             <p className="artefact-name">{recordName}</p>
             <p className="artefact-tags">TestTag</p>
-            <FsLightbox toggler={toggler} sources={[recordImg]} />
+            <FsLightbox toggler={toggler} sources={[
+'https://i.imgur.com/fsyrScY.jpg',
+'https://www.youtube.com/watch?v=3nQNiWdeH2Q',
+'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+]} />
           </div>
           <div>
             <div>{recordDescription}</div>
