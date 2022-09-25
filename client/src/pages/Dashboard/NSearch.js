94f83625d3de284955b439f68cd3632a3b7ca1ae
leonia <leonia@student.unimelb.edu.au>
2022-09-26 01:35:53 +1000

    retry deploy to heroku again

--- client/src/pages/Dashboard/NSearch.js
+++ client/src/pages/Dashboard/NSearch.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
-import { Link } from "react-router-dom";
 
 import "./ListView.css";
 
