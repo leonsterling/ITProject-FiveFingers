a7f954e14917b1a1fa86eb466115b5d1b8148568
leonia <leonia@student.unimelb.edu.au>
2022-09-26 01:25:30 +1000

    fixed some routing issues bug so it can run on heroku deployment

--- client/src/pages/EditPage/EditPage.js
+++ client/src/pages/EditPage/EditPage.js
@@ -87,7 +87,7 @@ const EditPage = () => {
   // Hook to get the data
   const configuration = {
     method: "get",
-    url: `http://localhost:5100/get-artefact/${_id}`,
+    url: `https://sterlingfamilyartefacts.herokuapp.com/get-artefact/${_id}`,
     headers: {
       Authorization: `Bearer ${token}`, // authorized route with jwt token
     },
