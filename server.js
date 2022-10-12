820cb5d46a46683a9401de5563df54794e0fbe6d
leonia <leonia@student.unimelb.edu.au>
2022-10-12 18:56:50 +1100

    deploying to heroku

--- server.js
+++ server.js
@@ -53,12 +53,12 @@ app.listen(process.env.PORT || 5100, () => {
 const path = require("path");
 
 // Step 1:
-//app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/src")));
 
 // Step 2:
-//app.get("*", function (request, response) {
-//  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-//});
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/src", "index.html"));
+});
 
 // connect mongoose index in models folder
 require('./models')
