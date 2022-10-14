9de37ffe439b3bb7e15fdc3e163139f93ad97be5
Vincent Kurniawan <vkurniawan@student.unimelb.edu.au>
2022-10-15 01:27:16 +1100

    Added CORS headers

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ app.use(
     })
 );
 */
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(cors());
 /*
 app.use((req,res,next)=>{
