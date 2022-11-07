bea46fd723610d0c10256ec22786833a4fe9c62a
Nadya Aurelia H <nadyaaureliah@gmail.com>
2022-11-07 20:06:22 +1100

    Fix visual bugs, edit delete popup.

--- client/src/components/SkewerDropDown.js
+++ client/src/components/SkewerDropDown.js
@@ -48,7 +48,7 @@ const SkewerDropDown = ({ _id, setUserData, currPageNum, setCurrPageNum, setNumP
       >
         <div className="delete-message-container" onClick={(e) => e.stopPropagation()}>
           <div className="message">
-            <span>Delete Artefact?</span>
+            <span>Are you sure you want to delete this artefact?</span>
           </div>
           <div className="button-choices">
             <button
